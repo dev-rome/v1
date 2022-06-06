bbf234c77b087061dfe6c33d35fb190d414a19b1
dev-rome <haynesjerome@gmail.com>
2022-06-06 12:55:01 -0400

    imported files and added sections to app. Also changed function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
+import IntroSection from "./section/Hero";
+import About from "./section/About";
+import Portfolio from "./section/Portfolio";
+import Contact from "./section/Contact";
+import "./App.css";
+import "animate.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <NavBar />
       </header>
-    </div>
+      <main>
+        <IntroSection />
+        <About />
+        <Portfolio />
+        <Contact />
+      </main>
+    </>
   );
 }
 
