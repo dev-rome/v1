860d509245fc079aeb7d3c829fc5968f161601ce
dev-rome <haynesjerome@gmail.com>
2022-06-06 12:59:54 -0400

    added data to use in portfolio section

---
+++ src/components/PortfolioData.js
@@ -0,0 +1,58 @@
+export const Featured = [
+    {
+      id: 1,
+      title: "Test Title",
+      img: "https://via.placeholder.com/300x200",
+    },
+    {
+      id: 2,
+      title: "Test Title",
+      img: "https://via.placeholder.com/300x200",
+    },
+    {
+      id: 3,
+      title: "Test Title",
+      img: "https://via.placeholder.com/300x200",
+    },
+    {
+      id: 4,
+      title: "Test Title",
+      img: "https://via.placeholder.com/300x200",
+    },
+    {
+      id: 5,
+      title: "Test Title",
+      img: "https://via.placeholder.com/300x200",
+    },
+  ];
+  
+  export const Frontend = [
+    {
+      id: 1,
+      title: "Test Title",
+      img: "https://via.placeholder.com/300x200",
+    },
+    {
+      id: 2,
+      title: "Test Title",
+      img: "https://via.placeholder.com/300x200",
+    },
+    {
+      id: 3,
+      title: "Test Title",
+      img: "https://via.placeholder.com/300x200",
+    },
+  ];
+  
+  export const GeneralAssembly = [
+    {
+      id: 1,
+      title: "Test Title",
+      img: "https://via.placeholder.com/300x200",
+    },
+    {
+      id: 2,
+      title: "Test Title",
+      img: "https://via.placeholder.com/300x200",
+    },
+  ];  
