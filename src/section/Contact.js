b3257adb23b17f7f19134c23a205c024fd6d6fe2
dev-rome <haynesjerome@gmail.com>
2022-06-07 12:44:39 -0400

    added new button and edit styles

--- src/section/Contact.js
+++ src/section/Contact.js
@@ -10,14 +10,25 @@ const Contact = () => {
         Currently available for hire, my inbox is always open. Also avaiable for
         freelance work.
       </p>
-      <a 
-        className="contact__btn"
-        href="mailto:haynesjerome@gmail.com"
-        rel="noreferrer" 
-    >
-        <i className="fa-solid fa-envelope contact__btn-icon"></i>
-        Contact
-      </a>
+      <div className="contact__btn-container">
+        <a
+          className="contact__btn"
+          href="mailto:haynesjerome@gmail.com"
+          rel="noreferrer"
+        >
+          <i className="fa-solid fa-envelope contact__btn-icon"></i>
+          Contact
+        </a>
+        <a
+          className="contact__btn"
+          href="https://eliteeyewebdesign.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fa-solid fa-link contact__btn-icon"></i>
+          Website
+        </a>
+      </div>
     </section>
   );
 };
