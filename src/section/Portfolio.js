2bd1bc030fa1b67a6a5cf9c5c01592ef1e1a20f7
dev-rome <haynesjerome@gmail.com>
2022-06-23 19:36:18 -0400

    added projects from project folder to portoflio section

--- src/section/Portfolio.js
+++ src/section/Portfolio.js
@@ -1,4 +1,7 @@
 import React from "react";
+import ProjectOne from "../projects/ProjectOne";
+import ProjectTwo from "../projects/ProjectTwo";
+import ProjectThree from "../projects/ProjectThree";
 import "./Portfolio.css";
 import { AnimationOnScroll } from "react-animation-on-scroll";
 
@@ -8,125 +11,9 @@ const Portfolio = () => {
       <section className="portfolio__container" id="portfolio">
         <h1 className="portfolio__title--small">Portfolio</h1>
         <ul className="portfolio__list--grid">
-          <li className="portfolio__item">
-            <header>
-              <div className="portfolio--top">
-                <a
-                  href="https://github.com/dev-rome/Tenant-Estate"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fab fa-github portfolio__github"></i>
-                </a>
-                <a
-                  href="https://enigmatic-anchorage-66839.herokuapp.com/buy"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fa-solid fa-arrow-right-to-bracket portfolio__arrow"></i>
-                </a>
-              </div>
-              <h3 className="portfolio__item--title">
-                <a
-                  href="https://enigmatic-anchorage-66839.herokuapp.com/buy"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="portfolio__item--white"
-                >
-                  Tenant-Estate
-                </a>
-              </h3>
-              <p className="portfolio__text">
-                Tenant-Estate is a home/apartment listing app that allows user
-                to rent or sell their property.
-              </p>
-            </header>
-            <footer>
-              <ul className="portfolio__tech">
-                <li className="portfolio__tech-item">EJS</li>
-                <li className="portfolio__tech-item">JavaScript</li>
-                <li className="portfolio__tech-item">HTML/CSS</li>
-                <li className="portfolio__tech-item">BootStrap</li>
-              </ul>
-            </footer>
-          </li>
-
-          <li className="portfolio__item">
-            <header>
-              <div className="portfolio--top">
-                <a
-                  href="https://github.com/dev-rome/dev-rome.github.io"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fab fa-github portfolio__github"></i>
-                </a>
-                <a
-                  href="https://dev-rome.github.io/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fa-solid fa-arrow-right-to-bracket portfolio__arrow"></i>
-                </a>
-              </div>
-              <h3 className="portfolio__item--title">
-                <a
-                  href="https://dev-rome.github.io/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="portfolio__item--white"
-                >
-                  Video Game Trivia
-                </a>
-              </h3>
-              <p className="portfolio__text">
-                Video game trivia is a game app for any player that wants to
-                test their knowledge video games.
-              </p>
-            </header>
-            <footer>
-              <ul className="portfolio__tech">
-                <li className="portfolio__tech-item">API</li>
-                <li className="portfolio__tech-item">JavaScript</li>
-                <li className="portfolio__tech-item">HTML/CSS</li>
-              </ul>
-            </footer>
-          </li>
-
-          <li className="portfolio__item">
-            <header>
-              <div className="portfolio--top">
-                <a
-                  href="https://eliteeyewebdesign.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fa-solid fa-arrow-right-to-bracket portfolio__arrow"></i>
-                </a>
-              </div>
-              <h3 className="portfolio__item--title">
-                <a
-                  href="https://eliteeyewebdesign.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="portfolio__item--white"
-                >
-                  Elite Eye Web Design
-                </a>
-              </h3>
-              <p className="portfolio__text">
-                Web design company that specializes in creating beautiful,
-                functional websites for small to large businesses.
-              </p>
-            </header>
-            <footer>
-              <ul className="portfolio__tech">
-                <li className="portfolio__tech-item">WordPress</li>
-                <li className="portfolio__tech-item">JavaScript</li>
-                <li className="portfolio__tech-item">HTML/CSS</li>
-              </ul>
-            </footer>
-          </li>
+          <ProjectOne />
+          <ProjectTwo />
+          <ProjectThree />
         </ul>
       </section>
     </AnimationOnScroll>
