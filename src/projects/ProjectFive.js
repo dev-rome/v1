2e4923b2d36215c3f1df78dad18bfe154bdec148
dev-rome <84602714+dev-rome@users.noreply.github.com>
2022-06-30 08:28:16 -0400

    update project five

--- src/projects/ProjectFive.js
+++ src/projects/ProjectFive.js
@@ -3,44 +3,50 @@ import "./ProjectFive.css";
 
 const ProjectFive = () => {
   return (
-    <li className="portfolio__item portfolio__item-five">
-      <header>
-        <div className="portfolio--top">
-          <a
-            href="https://github.com/dev-rome/NFT-Card-Component"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-github portfolio__github"></i>
-          </a>
-          <a
-            href="https://wizardly-ritchie-7bf14e.netlify.app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fa-solid fa-arrow-right-to-bracket portfolio__arrow"></i>
-          </a>
-        </div>
-        <h3 className="portfolio__item--title">
-          <a
-            href="https://wizardly-ritchie-7bf14e.netlify.appp"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="portfolio__item--white"
-          >
-            NFT Card Component
-          </a>
-        </h3>
-        <p className="portfolio__text">
-          Frontend mentor NFT Card Component project.
-        </p>
-      </header>
-      <footer>
-        <ul className="portfolio__tech">
-          <li className="portfolio__tech-item">HTML/CSS</li>
-        </ul>
-      </footer>
-    </li>
+   <a
+      href="https://wizardly-ritchie-7bf14e.netlify.app"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <li className="portfolio__item portfolio__item-five">
+        <header>
+          <div className="portfolio--top">
+            <a
+              href="https://github.com/dev-rome/NFT-Card-Component"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github portfolio__github"></i>
+            </a>
+            <a
+              href="https://wizardly-ritchie-7bf14e.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-solid fa-arrow-right-to-bracket portfolio__arrow"></i>
+            </a>
+          </div>
+          <h3 className="portfolio__item--title">
+            <a
+              href="https://wizardly-ritchie-7bf14e.netlify.appp"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="portfolio__item--white"
+            >
+              NFT Card Component
+            </a>
+          </h3>
+          <p className="portfolio__text">
+            Frontend mentor NFT Card Component project.
+          </p>
+        </header>
+        <footer>
+          <ul className="portfolio__tech">
+            <li className="portfolio__tech-item">HTML/CSS</li>
+          </ul>
+        </footer>
+      </li>
+    </a>
   );
 };
 
